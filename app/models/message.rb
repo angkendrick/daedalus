a5b1ea610fbfe107cc57a7d856f9ab5f7c2135b2
Ken Ang <angkendrick@gmail.com>
2015-07-25 20:16:14 +0000

    added update function to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
 
+  validates :player_id, presence: true
+  validates :position, presence: true
+  validates :level_number, presence: true
+  validates :content, presence: true
+
 end
